7c2745aa5938fa7ca203e6cd99f10732e8da7c62
wusili <498282023@qq.com>
2019-08-05 22:12:45 +0800

    1

---
+++ 2.js
@@ -0,0 +1,10 @@
+
+document.getElementById("switcher_plogin").onclick = function () {
+    var qlogin = document.getElementById("qlogin").style.display = "none";
+    var web_qr_login = document.getElementById("web_qr_login").style.display = "block";
+    document.getElementById("switcher_plogin").innerHTML = "";
+}
+document.getElementById("switch_btn_focus link_tips").onclick=function(){
+    var qlogin = document.getElementById("qlogin").style.display = "block";
+    var web_qr_login = document.getElementById("web_qr_login").style.display = "none";
+}
